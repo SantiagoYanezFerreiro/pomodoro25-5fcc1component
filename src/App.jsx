2286eb9115b5344c3b652a163b4ea154fe68f71f
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-13 01:12:54 +0200

    aligned items properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,70 @@
+import React from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+  const [breakTime, setBreakTime] = React.useState(5 * 60);
+  const [sessionTime, setSessionTime] = React.useState(25 * 60);
+  const [timerOn, setTimerOn] = React.useState(false);
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
 
-  return <div className="App">Hello World</div>;
+  const changeTime = (amount, type) => {
+    if (type === "break") {
+      setBreakTime((prev) => prev + amount);
+    } else {
+      if (sessionTime <= 60 && amount < 0) {
+        return;
+      }
+      setSessionTime((prev) => prev + amount);
+      if (!timerOn) {
+        setDisplayTime(sessionTime + amount);
+      }
+    }
+  };
+
+  return (
+    <div className="center-align">
+      <h1>25+5 Clock</h1>
+      <div className="dual-container">
+        <Length
+          title={"break-length"}
+          changeTime={changeTime}
+          type={"break"}
+          time={breakTime}
+          formatTime={formatTime}
+        />
+        <Length
+          title={"session-length"}
+          changeTime={changeTime}
+          type={"session"}
+          time={sessionTime}
+          formatTime={formatTime}
+        />
+      </div>
+      <h1>{formatTime(displayTime)}</h1>
+    </div>
+  );
+}
+function Length({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className="time-sets">
+        <button onClick={() => changeTime(+60, type)}>+</button>
+        <h3>{formatTime(time)}</h3>
+        <button onClick={() => changeTime(-60, type)}>-</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
